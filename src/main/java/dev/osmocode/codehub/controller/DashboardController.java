ba6d112c8610fc7c372e33874a546002f6276cbb
Sofian El Guetibi <sofian.elguetibi@gmail.com>
2023-03-12 19:22:24 +0100

    feat: dashboard page

--- src/main/java/dev/osmocode/codehub/controller/DashboardController.java
+++ src/main/java/dev/osmocode/codehub/controller/DashboardController.java
@@ -1,14 +1,32 @@
 package dev.osmocode.codehub.controller;
 
+import dev.osmocode.codehub.dto.UserProfileDto;
+import dev.osmocode.codehub.entity.User;
+import dev.osmocode.codehub.service.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class DashboardController {
+    
+    private final UserService userService;
 
+    public DashboardController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //TODO: Add pagination for followings
     @GetMapping("/dashboard")
-    public String getTemplate() {
-        return "dashboard";
+    public ModelAndView getTemplate(Authentication authentication) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("dashboard");
+        User user = userService.findUserByUserName(authentication.getName()).orElseThrow(() -> new RuntimeException(""));
+        modelAndView.addObject("followings", user.getFollowings().stream().map(UserProfileDto::buildFrom).collect(Collectors.toSet()));
+        return modelAndView;
     }
 
 }
