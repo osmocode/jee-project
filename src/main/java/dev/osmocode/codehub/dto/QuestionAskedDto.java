d65d66eb16a468a0e4819724d7c3d04e393bc6c3
Sofian El Guetibi <sofian.elguetibi@gmail.com>
2023-03-17 22:30:55 +0100

    feat: ask question

---
+++ src/main/java/dev/osmocode/codehub/dto/QuestionAskedDto.java
@@ -0,0 +1,29 @@
+package dev.osmocode.codehub.dto;
+
+import jakarta.validation.constraints.Size;
+
+public class QuestionAskedDto {
+
+    @Size(max = 150)
+    private String title;
+
+    @Size(max = 30_000)
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+}
